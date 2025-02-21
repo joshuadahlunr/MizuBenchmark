0565b3fac5c42b09d2474af7b574de64d6459971
Joshua Dahl <joshuadahl@nevada.unr.edu>
2025-02-20 16:53:14 -0800

    More Extensive Comparisons
    Add some of the more common interpreted/jit languages for comparison.

--- desktop.cpp
+++ desktop.cpp
@@ -37,4 +37,49 @@ int main() {
 
 		assert(*(env.memory + registers::a(0)) == 102334155);
 	});
+
+	ankerl::nanobench::Bench().run("Mizu(file + loading) fib(40)", []{
+		fp::raii::dynarray<opcode> fib_data; {
+			std::ifstream f("fib.mizu.bin", std::ios::binary);
+			f.seekg(0, std::ios::end);
+			size_t size = f.tellg();
+			fp::raii::dynarray data = fp::dynarray<std::byte>{}.resize(size);
+			f.seekg(0, std::ios::beg);
+			f.read((char*)data.data(), size);
+			fib_data = from_binary(data.view_full());
+		}
+		opcode* fib_file_program = (opcode*)fib_data.data();
+		assert(std::memcmp(fib_file_program, fib_program, sizeof(fib_program)) == 0);
+
+		registers_and_stack env = {};
+		setup_enviornment(env);
+
+		MIZU_START_FROM_ENVIORNMENT(fib_file_program, env);
+
+		assert(*(env.memory + registers::a(0)) == 102334155);
+	});
+
+	ankerl::nanobench::Bench().run("APE(native) fib(40)", []{
+		assert_with_side_effects(system("../fib.ape.com") != 0);
+	});
+
+	ankerl::nanobench::Bench().run("Node fib(40)", []{
+		assert_with_side_effects(system("node ../fib.js") != 0);
+	});
+
+	ankerl::nanobench::Bench().run("LuaJIT fib(40)", []{
+		assert_with_side_effects(system("luajit ../fib.lua") != 0);
+	});
+
+	ankerl::nanobench::Bench().run("Lua fib(40)", []{
+		assert_with_side_effects(system("lua ../fib.lua") != 0);
+	});
+
+	ankerl::nanobench::Bench().run("Python fib(40)", []{
+		assert_with_side_effects(system("python ../fib.py") != 0);
+	});
+
+	ankerl::nanobench::Bench().run("WASM3(c) fib(40)", []{
+		assert_with_side_effects(system("wasm3 ../fib.wasm") != 0);
+	});
 }
