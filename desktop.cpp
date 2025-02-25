9a05b1a8ab002fdd1ce92a0d9fdc149a576596fe
Joshua Dahl <joshuadahl@nevada.unr.edu>
2025-02-25 15:53:39 -0800

    Bubble Sort Test

--- desktop.cpp
+++ desktop.cpp
@@ -2,6 +2,7 @@
 
 #define MIZU_IMPLEMENTATION
 #include "fib_program.h"
+#include "bubble_program.h"
 #include <cstring>
 #include <fstream>
 #include <iostream>
@@ -60,26 +61,63 @@ int main() {
 	});
 
 	ankerl::nanobench::Bench().run("APE(native) fib(40)", []{
-		assert_with_side_effects(system("../fib.ape.com") != 0);
+		assert_with_side_effects(system("../fib.ape.com") == 0);
 	});
 
 	ankerl::nanobench::Bench().run("Node fib(40)", []{
-		assert_with_side_effects(system("node ../fib.js") != 0);
+		assert_with_side_effects(system("node ../fib.js") == 0);
 	});
 
 	ankerl::nanobench::Bench().run("LuaJIT fib(40)", []{
-		assert_with_side_effects(system("luajit ../fib.lua") != 0);
+		assert_with_side_effects(system("luajit ../fib.lua") == 0);
 	});
 
 	ankerl::nanobench::Bench().run("Lua fib(40)", []{
-		assert_with_side_effects(system("lua ../fib.lua") != 0);
+		assert_with_side_effects(system("lua ../fib.lua") == 0);
 	});
 
 	ankerl::nanobench::Bench().run("Python fib(40)", []{
-		assert_with_side_effects(system("python ../fib.py") != 0);
+		assert_with_side_effects(system("python ../fib.py") == 0);
 	});
 
 	ankerl::nanobench::Bench().run("WASM3(c) fib(40)", []{
-		assert_with_side_effects(system("wasm3 ../fib.wasm") != 0);
+		assert_with_side_effects(system("wasm3 ../fib.wasm") == 0);
+	});
+
+
+	std::cout << std::string(20, '-') << std::endl;
+
+
+	ankerl::nanobench::Bench().run("Mizu bubble(100)", []{
+		registers_and_stack env = {};
+		setup_enviornment(env);
+
+		MIZU_START_FROM_ENVIORNMENT(bubble_program, env);
+
+		assert(*(env.memory.end() - 1) == 1975);
+	});
+
+	ankerl::nanobench::Bench().run("APE(native) bubble(100)", []{
+		assert_with_side_effects(system("../bubble.ape.com") == 0);
+	});
+
+	ankerl::nanobench::Bench().run("Node bubble(100)", []{
+		assert_with_side_effects(system("node ../bubble.js") == 0);
+	});
+
+	ankerl::nanobench::Bench().run("LuaJIT bubble(100)", []{
+		assert_with_side_effects(system("luajit ../bubble.lua") == 0);
+	});
+
+	ankerl::nanobench::Bench().run("Lua bubble(100)", []{
+		assert_with_side_effects(system("lua ../bubble.lua") == 0);
+	});
+
+	ankerl::nanobench::Bench().run("Python bubble(100)", []{
+		assert_with_side_effects(system("python ../bubble.py") == 0);
+	});
+
+	ankerl::nanobench::Bench().run("WASM3(c) bubble(100)", []{
+		assert_with_side_effects(system("wasm3 ../bubble.wasm") == 0);
 	});
 }
